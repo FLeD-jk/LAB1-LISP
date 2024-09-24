a9ac385dee9e597296dd05f2cb9462ff5bf44084
FLeD-jk <122401625+FLeD-jk@users.noreply.github.com>
2024-09-24 19:47:26 +0300

    Update lab1.lisp

--- lab1.lisp
+++ lab1.lisp
@@ -4,55 +4,55 @@
 (print first-task)
 
 ; 2
-(print (CAR first-task))
+(print (cons first-task))
 
 ; 3
 (print (cdr first-task))
 
 ; 4
-(print (THIRD first-task))
+(print (third first-task))
 
 ; 5
-(print (LAST first-task))
+(print (car (last first-task)))
 
 ; 6.1.1
-(print (ATOM (NTH 1 first-task)))
+(print (atom (nth 1 first-task)))
 
 ; 6.1.2
-(print (ATOM (CDR first-task)))
+(print (atom (cdr first-task)))
 
 ; 6.1.3
-(print (ATOM (CAR (NTHCDR 2 first-task))))
+(print (atom (car (nthcdr 2 first-task))))
 
 ; 6.2.1
-(print (LISTP (SECOND (NTH 1 first-task))))
+(print (listp (second (nth 1 first-task))))
 
 ; 6.2.2
-(print (LISTP (FIRST first-task)))
+(print (listp (first first-task)))
 
 ; 6.2.3
-(print (LISTP (THIRD (CDR first-task))))
+(print (listp (third (cdr first-task))))
 
 ; 7.1.1
-(print (EQL (THIRD first-task) 7))
+(print (eql (third first-task) 7))
 
 ; 7.1.2
-(print (EQL (CDR first-task) '('C 'D)))
+(print (eql (cdr first-task) '('C 'D)))
 
 ; 7.2.1
-(print (EQUAL (CAR first-task) 'A))
+(print (equal (car first-task) 'A))
 
 ; 7.2.2
-(print (EQUAL (SECOND (NTH 1 first-task))(FIRST (LAST first-task))))
+(print (equal (second (nth 1 first-task))(first (last first-task))))
 
 ; 7.3.1
-(print (EQUALP (THIRD first-task) 7.0))
+(print (equalp (third first-task) 7.0))
 
 ; 7.3.2
-(print (EQUALP (CAR first-task) 'a))
+(print (equalp (car first-task) 'a))
 
 ; 8
-(print (APPEND first-task (SECOND first-task)))
+(print (append first-task (second first-task)))
 
 ; 15mod8 = 7
 (defvar sub-list nil)
